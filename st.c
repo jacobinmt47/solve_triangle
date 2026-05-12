8affe87979f1b634af25517cb33d19d17d6c130a
Jacob Lageschulte <jacobinmt47@gmail.com>
2026-05-12 11:00:13 -0700

    from float to double

--- st.c
+++ st.c
@@ -1,118 +1,118 @@
 #include<stdio.h>
 #include<math.h>
 
-float to_radian(float degrees) {
+double to_radian(double degrees) {
     return degrees * M_PI / 180.0;
 }
 
-float to_degree(float radians) {
+double to_degree(double radians) {
     return radians * 180.0 / M_PI;
 }
 
-void print_solved(float al, float bl, float cl, float Aa, float Ba, float Ca) {
+void print_solved(double al, double bl, double cl, double Aa, double Ba, double Ca) {
     printf("the triangle is fully solved\n");
-    printf("angle a %g side a: %g\n", Aa,al);
-    printf("angle b %g side b: %g\n", Ba,bl);
-    printf("angle c %g side c: %g\n", Ca,cl);
+    printf("angle a %lf side a: %lf\n", Aa,al);
+    printf("angle b %lf side b: %lf\n", Ba,bl);
+    printf("angle c %lf side c: %lf\n", Ca,cl);
 }
-float law_of_sine_angle(float al,float aa,float bl){
+double law_of_sine_angle(double al,double aa,double bl){
 // al is length of side a
 // aa is the angle oppisite of side a 
 // bl is the the length of side b
-    float ar = to_radian(aa);
-    float sr = sin(ar)/aa;
+    double ar = to_radian(aa);
+    double sr = sin(ar)/aa;
     return asin(sr*bl);
 }
-float law_of_sine_side(float al,float aa,float ba){
+double law_of_sine_side(double al,double aa,double ba){
 // al is length of side a
 // aa is the angle oppisite of side a 
 // ba is the the angle of side b
-    float ar = to_radian(aa);
-    float br = to_radian(ba);
+    double ar = to_radian(aa);
+    double br = to_radian(ba);
     return (al*sin(br))/sin(ar);
 }   
 
-float law_of_cosine_side(float al, float bl, float angle) {
+double law_of_cosine_side(double al, double bl, double angle) {
 // al is length of side a
 // bl is the length of side b
 // angle is the angle between sides a and b
-    float ar = to_radian(angle);
+    double ar = to_radian(angle);
     return sqrt(al*al + bl*bl - 2*al*bl*cos(ar));//length of side c
 }
 
-float law_of_cosine_angle(float al, float bl, float cl) {
+double law_of_cosine_angle(double al, double bl, double cl) {
 //solves for c
-    float c2 = cl*cl;
-    float sqrs = al*al+bl*bl;
-    float tba  =0-(2*al*bl);
-    float cc = (c2-sqrs) / tba;  //cosine of angle c
+    double c2 = cl*cl;
+    double sqrs = al*al+bl*bl;
+    double tba  =0-(2*al*bl);
+    double cc = (c2-sqrs) / tba;  //cosine of angle c
     return acos(cc);// the angle in radians
 }
 
-void solve_sss(float al,float bl, float cl){
-    float aa = law_of_cosine_angle(al,bl,cl);
-    float ba = law_of_sine_angle(al,aa,bl);
-    float ca = law_of_sine_angle(al,aa,al);
+void solve_sss(double al,double bl, double cl){
+    double aa = law_of_cosine_angle(al,bl,cl);
+    double ba = law_of_sine_angle(al,aa,bl);
+    double ca = law_of_sine_angle(al,aa,al);
     print_solved(al,bl,cl,to_degree(aa),to_degree(ba),to_degree(ca));
 }
 
-void solve_ssa(float al,float bl, float ca){
+void solve_ssa(double al,double bl, double ca){
     //solve side side angle
-    float cl = law_of_cosine_side(al,bl,ca);
-    float aa = law_of_sine_angle(cl,ca,al);
-    float ba = law_of_sine_angle(cl,ca,bl);
+    double cl = law_of_cosine_side(al,bl,ca);
+    double aa = law_of_sine_angle(cl,ca,al);
+    double ba = law_of_sine_angle(cl,ca,bl);
     print_solved(al,bl,cl,to_degree(aa),to_degree(ba),ca);
 }
 
-void solve_sas(float al,float aa,float bl){
-    float ba = law_of_sine_angle(al,aa,bl);
-    float bda = to_degree(ba);
-    float ca = 180 -(bda+aa); // ca in degrees
-    float cl = law_of_sine_side(al,aa,ca);
+void solve_sas(double al,double aa,double bl){
+    double ba = law_of_sine_angle(al,aa,bl);
+    double bda = to_degree(ba);
+    double ca = 180 -(bda+aa); // ca in degrees
+    double cl = law_of_sine_side(al,aa,ca);
     print_solved(al,bl,cl,to_degree(aa),to_degree(ba),ca);
 }
 int main(int argc,char** argv) {
 
 //declare variables for the triangle
-    float al = 0; // for length of side a
-    float bl = 0; // for length of side b
-    float cl = 0; // for length of side c
+    double al = 0; // for length of side a
+    double bl = 0; // for length of side b
+    double cl = 0; // for length of side c
 
-    float Aa = 0; // for angle a
-    float Ba = 0;
-    float Ca = 0;
+    double Aa = 0; // for angle a
+    double Ba = 0;
+    double Ca = 0;
 
 // get the values from the user
     int angles = 0;
     int sides = 0;
     printf("enter values for the triangle use zero if you don't know the value\n");
     printf("enter length of side a: ");
-    scanf("%G",&al);
+    scanf("%lf",&al);
     if(al != 0.0) {
         sides++;
     }
     printf("enter angle of a in degrees: ");
-    scanf("%G",&Aa);
+    scanf("%lf",&Aa);
     if(Aa != 0.0) {
         angles++;
     }
     printf("enter length of side b: ");
-    scanf("%G",&bl);
+    scanf("%lf",&bl);
     if(bl != 0.0) {
         sides++;
     }
     printf("enter angle of b in degrees: ");
-    scanf("%G",&Ba);
+    scanf("%lf",&Ba);
     if(Ba != 0.0) {
         angles++;
     }
     printf("enter length of side c: ");
-    scanf("%G",&cl);
+    scanf("%lf",&cl);
     if(cl != 0.0) {
         sides++;
     }
     printf("enter angle of c in degrees: ");
-    scanf("%G",&Ca);
+    scanf("%lf",&Ca);
     if(Ca != 0.0) {
         angles++;
     }
@@ -129,5 +129,17 @@ int main(int argc,char** argv) {
         printf("the triangle is fully solved\n");
         print_solved(al, bl, cl, Aa, Ba, Ca);
     }
+
+    if(sides == 3 && angles == 0){
+        solve_ssa(al,bl,cl);
+    }
+    if(sides>=2 && angles >=1){
+        printf("figure out if its a ssa triangle or a sas")
+        return 0;
+    }
+    if(sides>=1 && angles >=2){
+        printf("solve a aas triangle");
+    }
+
     return  0;
 }
