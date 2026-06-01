30c5ac4f46e3d0abffb31d4459115e34a0327505
jacob <jacobinmt47@gmail.com>
2026-05-31 21:09:46 -0700

    added sas v1

--- st.c
+++ st.c
@@ -71,7 +71,7 @@ void solve_ssa(double al,double bl, double ca) {
 
 void solve_sas(double al,double aa,double bl) {
     double ba = law_of_sine_angle(al,aa,bl);
-    double bda = to_degree(ba);
+    double bda = to_degree(ba); // law of sine returns in radians
     double ca = 180 -(bda+aa); // ca in degrees
     double cl = law_of_sine_side(al,aa,ca);
     print_solved(al,bl,cl,to_degree(aa),to_degree(ba),ca);
@@ -140,6 +140,7 @@ int main(int argc,char** argv) {
         print_solved(al, bl, cl, Aa, Ba, Ca);
         return 0;
     }
+// start processing data here
 
     if(sides == 3 && angles == 0) {
         solve_sss(al,bl,cl);
@@ -149,95 +150,112 @@ int main(int argc,char** argv) {
         printf("figure out if its a ssa triangle or a sas triangle \n ");
         if(al>0 && Aa >0) {
             printf("Aa is a side angle pair\n");
+            if(bl != 0) {
+                solve_sas(al,Aa,bl);
+            }
+            else {
+                solve_sas(al,Aa,cl);
+            }
             return 0;
         }
         if(bl>0 && Ba >0) {
             printf("Ba is the side angle pair\n");
+            if(al !=0) {
+                solve_sas(bl,Ba,al);
+            }
+            else {
+                solve_sas(bl,Ba,cl);
+            }
             return 0;
         }
         if(cl>0 &&  Ca >0) {
             printf("Ca us the side angle pair\n");
+            if(al != 0) {
+                solve_sas(cl,Ca,al);
+                else {
+                    solve_sas(cl,Ca,bl);
+                }
+                return 0;
+            }
+            printf("it's a ssa triangle\n");
+            int side_count = 0;
+            double sides[2] = { 0,0 };
+            double angle = 0.0;
+            if(al > 0) {
+                sides[side_count]  = al;
+                side_count++;
+            }
+            if( bl > 0) {
+                sides[side_count] = bl;
+                side_count++;
+            }
+            assert(side_count>0);
+            // count cl if bl or al haven't been counted
+            if( cl > 0 && side_count<2) {
+                sides[1] = cl;
+            }
+            if(Aa > 0 ) {
+                angle = Aa;
+            }
+            if(Ba > 0 ) {
+                angle = Ba;
+            }
+            if(Ca > 0 ) {
+                angle = Ca;
+            }
+            assert(angle != 0.0);
+            solve_ssa(sides[0],sides[1],angle);
+            return 0;
+
+
+        }
+        if(sides>=1 && angles >=2) {
+            printf("solve a aas triangle");
+            int angle_count = 0;
+            double angles[2] = {0, 0};
+            double side_length = 0.0;
+            if(Aa != 0.0) {
+                angles[angle_count] = Aa;
+                angle_count++;
+            }
+
+            if(Ba != 0.0) {
+                angles[angle_count] = Ba;
+                angle_count++;
+            }
+            if(angle_count<2 && Ca != 0.0) {
+                angles[1] = Ca;
+            }
+
+            //TODO: create function to solve aas triangle
+            // Find angle side pair if they exists
+
+            if (Aa != 0.0 && al != 0.0) {
+                printf("Aa is angle pair");
+                //TODO solve aas
+                return 0;
+            }
+            if (Ba != 0.0 && bl != 0.0) {
+                printf("Ba is angle pair");
+                //TODO solve for Ba aas
+                return 0;
+            }
+            if (Ca != 0.0 && cl != 0.0) {
+                printf("Ba is angle pair");
+                //TODO solve for Ca aas
+                return 0;
+            }
+            if (al != 0.0) {
+                solve_aas_nopair(Aa, Ba, Ca, al);
+            }
+            if (bl != 0.0) {
+                solve_aas_nopair(Aa, Ba, Ca, bl);
+            }
+            if (cl != 0.0) {
+                solve_aas_nopair(Aa, Ba, Ca, cl);
+            }
             return 0;
         }
-        printf("it's a ssa triangle\n");
-        int side_count = 0;
-        double sides[2] = { 0,0 };
-        double angle = 0.0;
-        if(al > 0) {
-            sides[side_count]  = al;
-            side_count++;
-        }
-        if( bl > 0) {
-            sides[side_count] = bl;
-            side_count++;
-        }
-        assert(side_count>0);
-        // count cl if bl or al haven't been counted
-        if( cl > 0 && side_count<2) {
-            sides[1] = cl;
-        }
-        if(Aa > 0 ) {
-            angle = Aa;
-        }
-        if(Ba > 0 ) {
-            angle = Ba;
-        }
-        if(Ca > 0 ) {
-            angle = Ca;
-        }
-        assert(angle != 0.0);
-        solve_ssa(sides[0],sides[1],angle);
-        return 0;
 
-
+        return  0;
     }
-    if(sides>=1 && angles >=2) {
-        printf("solve a aas triangle");
-        int angle_count = 0;
-        double angles[2] = {0 , 0};
-        double side_length = 0.0;
-        if(Aa != 0.0) {
-            angles[angle_count] = Aa;
-            angle_count++;
-        }
-
-        if(Ba != 0.0) {
-            angles[angle_count] = Ba;
-            angle_count++;
-        }
-        if(angle_count<2 && Ca != 0.0) {
-            angles[1] = Ca;
-        }
-
-        //TODO: create function to solve aas triangle
-        // Find angle side pair if they exists
-
-        if (Aa != 0.0 && al != 0.0) {
-            printf("Aa is angle pair");
-            //TODO solve aas
-            return 0;
-        }
-        if (Ba != 0.0 && bl != 0.0) {
-            printf("Ba is angle pair");
-            //TODO solve for Ba aas
-            return 0;
-        }
-        if (Ca != 0.0 && cl != 0.0) {
-            printf("Ba is angle pair");
-            //TODO solve for Ca aas
-            return 0;
-        }
-        if (al != 0.0) {
-            solve_aas_nopair(Aa, Ba, Ca, al);
-        }
-        if (bl != 0.0) {
-            solve_aas_nopair(Aa, Ba, Ca, bl);
-        }
-        if (cl != 0.0) {
-            solve_aas_nopair(Aa, Ba, Ca, cl);
-        }
-        return 0;
-    }
-
-    return  0;
-}
